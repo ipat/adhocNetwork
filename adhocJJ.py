e0e4156e687066faf481d0371bed1c216bc680bf
sakonporn <juckjick14@gmail.com>
2015-11-03 21:54:33 +0700

    add function receive file

--- adhocJJ.py
+++ adhocJJ.py
@@ -3,14 +3,15 @@ import getpass
 import subprocess
 
 #get user password
-print "enter your password"
-password = getpass.getpass()
+# print "enter your password"
+# password = getpass.getpass()
 #print password
 
 # bashCommand = "sudo pwd"
 # process = subprocess.call(bashCommand.split())
 # output = process.communicate()[0]
 # print output
+
 from random import randint
 x = randint(0,255)
 print x
@@ -29,6 +30,35 @@ bashCommand = "sudo ifconfig wlan0 192.168.1." + str(x)
 process = subprocess.call(bashCommand.split())
 bashCommand = "sudo iwconfig wlan0 channel 4"
 process = subprocess.call(bashCommand.split())
+
+#receive file
+from socket import *
+import sys
+import select
+
+host = "0.0.0.0"
+port = 9999
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind((host,port))
+
+addr = (host,port)
+buf = 9000000
+
+data,addr = s.recvfrom(buf)
+print "Received File",data.strip()
+f = open(data.strip(),'wb')
+
+data,addr = s.recvfrom(buf)
+try:
+    while (data):
+        f.write(data)
+        s.settimeout(2)
+        data,addr = s.recvfrom(buf)
+except timeout:
+    f.close()
+    s.close()
+    print "File Downloaded"
+#end of receiving
 
 # bashCommand = "pwd"
 # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
