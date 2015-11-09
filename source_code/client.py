b8017b96121b4a00cde090bff987c9e99e8165fc
Pat <pat.hornchaiya.com>
2015-11-09 13:39:20 +0700

    Cleaning code

---
+++ source_code/client.py
@@ -0,0 +1,70 @@
+import getpass
+import subprocess
+from socket import *
+import sys
+import select
+import cv2
+import time
+
+# Random IP
+from random import randint
+x = randint(0,255)
+print x
+# Setup ad-hoc network
+bashCommand = "sudo service network-manager stop"
+process = subprocess.call(bashCommand.split())
+bashCommand = "sudo ifconfig wlan0 down"
+process = subprocess.call(bashCommand.split())
+bashCommand = "sudo iwconfig wlan0 mode ad-hoc"
+process = subprocess.call(bashCommand.split())
+bashCommand = "sudo iwconfig wlan0 essid 'car4cast'"
+process = subprocess.call(bashCommand.split())
+bashCommand = "sudo iwconfig wlan0 key 1234567890"
+process = subprocess.call(bashCommand.split())
+bashCommand = "sudo ifconfig wlan0 192.168.1." + str(x)
+process = subprocess.call(bashCommand.split())
+bashCommand = "sudo iwconfig wlan0 channel 4"
+process = subprocess.call(bashCommand.split())
+
+# Config socket
+host = "0.0.0.0"
+port = 9999
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind((host,port))
+s.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
+
+# Set address
+addr = (host,port)
+buf = 100000
+prev_seq_no = -1
+
+
+# Infinite loop for receiving the images
+while True:
+    f = open("test.jpg",'wb')
+    startWrite = False
+    data,addr = s.recvfrom(buf)
+    try:
+        seq_no = int(data[:4])
+        hop = int(data[5])
+        if(seq_no > prev_seq_no or seq_no < prev_seq_no - 5000) :
+            print data[:5]
+            print "Sequence No = " + str(seq_no)
+            prev_seq_no = seq_no
+            data = data[5:]
+            current_seq = (str(seq_no)).zfill(4)
+            f.write(data)
+            f.close()
+            img = cv2.imread('test.jpg')
+            res = cv2.resize(img,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+
+            if img is not None:
+                cv2.imshow('image',res)
+                k = cv2.waitKey(10)
+            data = current_seq + str(hop + 1) + data
+            s.sendto(data,('192.168.1.255',9999))
+
+    except timeout:
+        f.close()
+        s.close()
+        print "File Downloaded"
---
+++ source_code/host.py
@@ -0,0 +1,89 @@
+import sys, time
+from socket import *
+import cv2.cv as cv
+import cv2
+import time
+import numpy as np
+import subprocess
+
+# Setup ad-hoc network
+bashCommand = "sudo service network-manager stop"
+process = subprocess.call(bashCommand.split())
+
+bashCommand = "sudo ifconfig wlan0 down"
+process = subprocess.call(bashCommand.split())
+
+bashCommand = "sudo iwconfig wlan0 mode ad-hoc"
+process = subprocess.call(bashCommand.split())
+
+bashCommand = "sudo iwconfig wlan0 essid 'car4cast'"
+process = subprocess.call(bashCommand.split())
+
+bashCommand = "sudo iwconfig wlan0 key 1234567890"
+process = subprocess.call(bashCommand.split())
+
+# Random IP
+from random import randint
+_randomedIP = (randint(0,255))
+print _randomedIP
+bashCommand = "sudo ifconfig wlan0 192.168.1." + str(_randomedIP)
+process = subprocess.call(bashCommand.split())
+
+bashCommand = "sudo iwconfig wlan0 channel 4"
+process = subprocess.call(bashCommand.split())
+
+
+# Set port
+MYPORT = 9999
+# Set Address
+addr = ('192.168.1.255', MYPORT)
+# Set buffer
+buf = 80000
+
+# Config socket
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(('', 0))
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+# Create windows for showing the image
+cv.NamedWindow("w1", cv.CV_WINDOW_NORMAL)
+camera_index = 0
+capture = cv.CaptureFromCAM(camera_index)
+# Set capture resolution
+cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 120)
+cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 160)
+
+# Subsequence for capturing the image
+def repeat():
+    global capture
+    global camera_index
+    frame = cv.QueryFrame(capture)
+
+    cv.ShowImage("w1", frame)
+    cv.SaveImage("test.jpg",frame)
+    c = cv.WaitKey(10)
+
+# Set initial sequence number
+seq_num = 0;
+
+# Infinite loop for broadcasting images
+while 1:
+    data = ""
+    i = 0
+    # Call repeat() to capture the image
+    repeat()
+    # Send image data
+    while data:
+        f=open("test.jpg","rb")
+        # Append sequence number and hop number to the data
+        data = (str(seq_num%10000)).zfill(4)  + "0" + f.read(buf)
+        # Print current image status
+        print data[:5]
+
+        if(s.sendto(data,addr)):
+            data = f.read(buf)
+    # Add sequence number by 1
+    seq_num += 1;
+    print "Image was sent."
+
+    time.sleep(0.05)
